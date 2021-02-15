69ed4e83f4a728e41b73d963680a7f19b1c48946
mustakim79 <mustakimkrs@gmail.com>
2021-02-15 09:47:12 +0530

    initial commit

---
+++ App_Code/Connection_class.cs
@@ -0,0 +1,23 @@
+﻿
+/// <summary>
+/// Summary description for Connection_class
+/// </summary>
+using System.Configuration;
+using System.Data.SqlClient;
+public class Connection_class
+{
+    public Connection_class()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public static SqlConnection getCon()
+    {
+
+        string str = ConfigurationManager.ConnectionStrings["sqlcon"].ConnectionString;
+        SqlConnection con = new SqlConnection(str);
+        con.Open();
+        return con;
+    }
+}
